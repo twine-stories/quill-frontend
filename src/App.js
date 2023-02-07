8a0d0b6827cc4d4a292e69fbabc4aaa91f12d6ef
Rithik <Jain>
2023-02-06 23:42:43 -0600

    basic site no web scaling

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img id="header" src="twine.png" alt="Twine" />
+        <div className='content'>
+            <div className="left">
+                <p>self-publishing platform for authors and illustrators to collab, profit-share & retain 90%+ of revenue</p>
+                <a href="mailto:info@twinestories.com" id="wrapper">
+                    <img id="mail" src="mail.png" alt="mail!" />
+                    <div id="email" className="purple">info@twinestories.com</div>
+                </a>
+            </div>
+            <div className="right">
+                <img id="digging" src="digging.png" alt="coming soon!" />
+                <p id="comingSoon" className="purple">coming soon!</p>
+            </div>
+        </div>
     </div>
   );
 }
