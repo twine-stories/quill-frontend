349829464034a71b0a38c48e4d2a15e70ffd135a
Rithik <Jain>
2023-04-04 13:58:42 -0500

    fixed image stuff

--- src/components/Collaborator.tsx
+++ src/components/Collaborator.tsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import TwineButton from '../components/TwineButton.tsx';
-import { Input } from '@mui/joy';
+import { Input, IconButton } from '@mui/joy';
 import { CollaboratorContext } from '../pages/Create.tsx';
 
 interface CollaboratorProps {
     defaultCreator: string;
+    defaultWallet: string;
     defaultProfit: number;
     principle: boolean;
     id: number;
@@ -15,11 +15,22 @@ function Collaborator(props: CollaboratorProps) {
     const context: object = useContext(CollaboratorContext);
     const remove: (id: number) => void = context['remove'];
 
+    const creatorName: string = props.principle ? 'Principle Creator' : 'Creator Name';
+    const creatorWallet: string = props.principle ? 'Your Wallet' : 'Collaborator\'s Wallet';
+
     return (
         <div className='collaborators'>
-            <Input placeholder='Creator Name' defaultValue={props.defaultCreator}/>
+            <Input placeholder={creatorName} defaultValue={props.defaultCreator}/>
             <Input placeholder='Profit Percentage' defaultValue={props.defaultProfit}/>
-            {!props.principle && <TwineButton name='Remove' action={() => {remove(props.id)}} />}
+            <Input placeholder={creatorWallet} defaultValue={props.defaultWallet}/>
+            {!props.principle && <IconButton sx={{ '&:hover': {
+                backgroundColor: 'black',
+            } }} variant="plain" onClick={() => {remove(props.id)}}>
+                <img
+                    src="icons/remove.svg"
+                    alt=""
+                />
+            </IconButton>}
         </div>
     )
 
