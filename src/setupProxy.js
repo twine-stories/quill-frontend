4721c04e834f1c1154a525d9ed796be6fee35ae9
Rithik <Jain>
2023-04-08 23:33:56 -0500

    reverted proxy changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-import { env } from './config.ts';
 
-const prodProxy = (app) => {
-    // put prod urls here
-}
-
-const devProxy = (app) => {
+module.exports = function(app) {
     app.use(createProxyMiddleware("/api", { target: "http://localhost:8080" }));
     app.use(createProxyMiddleware("/algo", { target: "http://localhost:8000" }));
-}
-
-module.exports = env === 'dev' ? devProxy : prodProxy;
+};
