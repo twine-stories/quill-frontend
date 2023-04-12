9857be5adcbb1831b24a21a0346336499e2ac11c
BCassady <bryant.cassady@gmail.com>
2023-04-12 17:07:00 -0500

    fix generic profile

--- src/pages/GenericProfile.tsx
+++ src/pages/GenericProfile.tsx
@@ -8,6 +8,7 @@ import "../components/ProfileSidebar.tsx"
 import ProfileSidebar from '../components/ProfileSidebar.tsx';
 import {Button} from "@mui/joy";
 import { genericGet } from '../utils/api.ts';
+import { PROFILE_IMGS_BUCKET } from '../config.ts';
 
 const axios = require('axios').default;
 
@@ -57,18 +58,20 @@ function GenericProfile() {
             <Navbar />
             <div className="profile-page">
                 <div className="profile-info">
-                    <img
-                        src = {user && user.profileImg}
+                <img
+                        src = {user && 'https://'+PROFILE_IMGS_BUCKET+'.s3.amazonaws.com/'+user.profileImg}
                         alt = ""
+                        width = "128"
+                        height = "128"
                         onError={e => {
-                            e.currentTarget.src = "https://placehold.co/100x100"
+                            e.currentTarget.src = 'https://'+PROFILE_IMGS_BUCKET+'.s3.amazonaws.com/default.jpeg'
                         }}
                     />
                     <div className="name-username">
                         <h2 >@{user && user.userName}</h2>
                         <h1 >{user && user.firstName} {user && user.lastName}</h1>
                         <div className="edit-notif">
-                            <Button href="/edit-profile" className="edit-profile-btn" onClick={editProfile}>Edit Profile</Button>
+                            <Button href="/edit-profile" className="edit-profile-btn" onClick={editProfile}>Follow</Button>
                             <Button className="notif-btn">Bell</Button>
                         </div>
                         <p > {user && user.description}</p>
