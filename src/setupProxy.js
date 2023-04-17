357c47f034bcc816ce1666c45b2edd832cbf3d03
Rithik <Jain>
2023-04-17 04:34:38 -0500

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware("/api", { target: "http://localhost:8080" }));
+    app.use(createProxyMiddleware("/api", { target: "http://ec2-3-138-103-20.us-east-2.compute.amazonaws.com:8080" }));
     app.use(createProxyMiddleware("/algo", { target: "http://localhost:8000" }));
 };
