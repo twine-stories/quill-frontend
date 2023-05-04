486a992231ee121e608221ff96fe1f5d5b856d0a
BCassady <bryant.cassady@gmail.com>
2023-05-04 00:23:46 -0500

    Search is cooler now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,12 @@ import { UserContext } from "../App.tsx";
 import LoginWall from './LoginWall.tsx';
 import RegisterCreator from './RegisterCreator.tsx';
 import ClickProfile from './ClickProfile.tsx';
-import { Autocomplete } from '@mui/joy';
+import { Autocomplete, AutocompleteOption, Typography, ListItemContent } from '@mui/joy';
 import IconButton from './IconButton.tsx';
-import { Work } from '../utils/types.ts';
+import { Work, User} from '../utils/types.ts';
 import { genericGet } from '../utils/api.ts';
 import { useNavigate } from 'react-router-dom';
+import { StyledAutocompleteListbox } from '@mui/joy/AutocompleteListbox/AutocompleteListbox.js';
 
 const NavDiv = styled.div`
     display: flex;
@@ -34,15 +35,20 @@ const NavDiv = styled.div`
     }  
 `;
 
+
+type dyad = {
+    isWork: boolean;
+    name: string;
+    link: string;
+}
+
 function Navbar() {
     const context: object = useContext(UserContext);
     const [open, setOpen] = useState<boolean>(false);
     const [openCreator, setOpenCreator] = useState<boolean>(false);
     const [openCollab, setOpenCollab] = useState<boolean>(false);
     const [isSearching , setIsSearching] = useState<boolean>(false);
-    const [allWorks, setAllWorks] = useState<Work[]>([]);
-    const [workStrings, setWorkStrings] = useState<string[]>([]);
-    const [mapStringToLink, setMapStringToLink] = useState<Map<string, string>>(new Map<string, string>());
+    const [dyads, setDyads] = useState<dyad[]>([]);
 
     const navigate = useNavigate();
 
@@ -81,31 +87,35 @@ function Navbar() {
         setIsSearching(false);
     }
 
-    const onChange = async (e: React.SyntheticEvent<Element, Event>, value: string | null) => {
+    const onChange = async (e: React.SyntheticEvent<Element, Event>, value: string | dyad | null) => {
         console.log(value);
-        if (value !== null) {
-            let link: string = mapStringToLink.get(value) || "";
-            if (link) {
-                navigate('/story/' + link);
-            }
+        if (value !== null && typeof value !== 'string') {
+            navigate(value.link);
         }
     }
 
 
     useEffect(() => {
-        genericGet("/api/works").then((res) => {
-            setAllWorks(res);
-            let workNames: string[] = [];
-            let map: Map<string, string> = new Map<string, string>();
-            res.forEach((work: Work) => {
-                workNames.push(work.title);
-                map.set(work.title, work.url);
-            });
-            setWorkStrings(workNames);
-            setMapStringToLink(map);
+        const getDyads = async () => {
+            const response1 = await genericGet('/api/works');
+            const response2 = await genericGet('/api/users');
+            const newDyads: dyad[] = [];
+            if (response1){ 
+                response1.forEach((work: Work) => {
+                    newDyads.push({isWork: true, name: work.title, link: `/story/${work.url}`});
+                });
+            }
+            if (response2){
+                response2.forEach((user: User) => {
+                    newDyads.push({isWork: false, name: user.userName, link: `/profile/${user.userName}`});
+                });
+            }
+            setDyads(newDyads);
 
-        });
+        }
 
+        getDyads();
+        
     }, []);
 
     return (
@@ -121,7 +131,27 @@ function Navbar() {
                         <a onClick={createNav}>create</a>
                         {
                             isSearching ?
-                            <Autocomplete options = {workStrings}  freeSolo={true} onClose={closeSearch} onChange={onChange}
+                            <Autocomplete autoHighlight sx= {{width: "225px"}} options = {dyads} freeSolo={true} onClose={closeSearch} onChange={onChange} 
+                            getOptionLabel={(option: string | dyad) => {
+                                if (typeof option === 'string') {
+                                    return option;
+                                    } else {
+                                        return option.name;
+                                    }
+                                }
+                            }
+                            renderOption={(props, option) => {
+                                return (
+                                    <AutocompleteOption {...props}>
+                                    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'left', gap: '0px'}}>
+                                        {option.name}
+                                        <Typography level="body3" sx={{margin: '1px'}}>
+                                        {option.isWork ? "Story" : "User"}
+                                        </Typography>
+                                    </div>
+                                    </AutocompleteOption>
+                                )
+                            }}
                             />
                             :
                             <IconButton action={openSearch} icon='/icons/search.svg' color="green" />
