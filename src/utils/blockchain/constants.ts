681a46df3ca92263916aaaeb930a044e5cf0278e
Rithik <Jain>
2023-06-04 16:07:10 -0400

    send tips to backend db

--- src/utils/blockchain/constants.ts
+++ src/utils/blockchain/constants.ts
@@ -6,7 +6,6 @@ export const BUY: Uint8Array = encoder.encode("buy");
 export const STOP_SELL_OFFER: Uint8Array = encoder.encode("stopSellOffer");
 export const MAKE_PAYMENTS: Uint8Array = encoder.encode("makePayments");
 
-
 /*
  * App States
  * 0: not initialized
@@ -24,3 +23,11 @@ export const nameMapping: object = {
     'START_TIME': 'startTime',
     'DURATION': 'duration'
 };
+
+export const TWINE_CUT = 0.1;
+export const microToAlgo = (micro_algo: bigint): number => {
+    return Number(micro_algo / 1000000n) + Number (micro_algo % 1000000n);
+}
+export const algoToMicro = (algo: number): bigint => {
+    return BigInt(algo) * 1000000n;
+}
