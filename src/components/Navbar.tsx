abdc9eb47d5dafd68f00b7faace94e6d9dea6fe6
loveheenavasu <trivediheenalove@gmail.com>
2023-06-28 11:31:05 +0530

    changes it to margin from padding in menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -156,8 +156,8 @@ function Navbar() {
                     </Box>
                     <Box className='navItemsNames'>
                         <a href="/art">art</a>
-                        <a onClick={() => setOpenCollab(true)} style={{paddingLeft: "40px",paddingRight: "40px"}}>collab</a>
-                        <a onClick={createNav} style={{paddingRight:"30px"}}>create</a>
+                        <a onClick={() => setOpenCollab(true)} style={{marginLeft: "40px",marginRight: "40px"}}>collab</a>
+                        <a onClick={createNav} style={{marginRight:"30px"}}>create</a>
                     </Box>
                     <Box className='iconSearch navItemsNames'>
                         {searchIcon}
