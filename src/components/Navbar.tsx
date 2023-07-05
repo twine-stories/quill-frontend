debf0b94fd8a759fbd27f6a89c4c9d6b4c76f696
loveheenavasu <trivediheenalove@gmail.com>
2023-07-05 10:20:02 +0530

    Fix:styling and responsive of BetaLandingPage and all the screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -165,9 +165,9 @@ function Navbar() {
                         </a>
                     </Box>
                     <Box className='navItemsNames'>
-                        <a href="/art" style={{marginLeft:"30px"}} className="navLink">art</a>
-                        <a onClick={() => setOpenCollab(true)} style={{margin:"0px 40px"}} className="navLink">collab</a>
-                        <a onClick={createNav} style={{marginRight:"30px"}} className="navLink">create</a>
+                        <a href="/art" style={{marginLeft:"30px"}}>art</a>
+                        <a onClick={() => setOpenCollab(true)} style={{margin:"0px 40px"}}>collab</a>
+                        <a onClick={createNav} style={{marginRight:"30px"}}>create</a>
                     </Box>
                     <Box className='iconSearch navItemsNames'>
                         {searchIcon}
