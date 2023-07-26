ec5131f621984c40f2038704285c846c6a41598c
loveheenavasu <trivediheenalove@gmail.com>
2023-07-26 13:29:32 +0530

    create common for search field and fix responsvie issue

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -4,25 +4,31 @@ import styled from "styled-components";
 import { UserContext } from "../App.tsx";
 import { Work, User } from '../utils/types.ts';
 import { genericGet } from '../utils/api.ts';
+import CollabPopup from './CollabPopup.tsx';
 import Button from '@mui/joy/Button';
 import Menu from '@mui/joy/Menu';
 import MenuItem from '@mui/joy/MenuItem';
 import IconButton from './IconButton.tsx';
 import "./Hamburger.css"
+import Search from './Search.tsx';
+
+// interface SearchStylingprops {
+//    className?: string;
+// }
 
 
-type dyad = {
-    isWork: boolean;
-    name: string;
-    link: string;
-}
+// type dyad = {
+//     isWork: boolean;
+//     name: string;
+//     link: string;
+// }
 
-const Hamburger = () => {
+const Hamburger = (props:SearchStylingprops) => {
     const [openCollab, setOpenCollab] = useState<boolean>(false);
     const [openCreator, setOpenCreator] = useState<boolean>(false);
     const context: object = useContext(UserContext);
     const [isSearching, setIsSearching] = useState<boolean>(false);
-    const [dyads, setDyads] = useState<dyad[]>([]);
+    // const [dyads, setDyads] = useState<dyad[]>([]);
     const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
     const handleToggle = () => {
         setMenuOpen(!isMenuOpen)
@@ -32,9 +38,9 @@ const Hamburger = () => {
     const buttonRef = React.useRef(null);
     const [open, setOpen] = React.useState(false);
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+    // const handleClose = () => {
+    //     setOpen(false);
+    // };
 
     const navToCreate = () => {
         setOpenCreator(false);
@@ -56,76 +62,78 @@ const Hamburger = () => {
     const closeSearch = async () => {
         setIsSearching(false);
     }
-    const onChange = async (e: React.SyntheticEvent<Element, Event>, value: string | dyad | null) => {
-        console.log(value);
-        if (value !== null && typeof value !== 'string') {
-            window.location.href = value.link;
-        }
-    }
+    // const onChange = async (e: React.SyntheticEvent<Element, Event>, value: string | dyad | null) => {
+    //     console.log(value);
+    //     if (value !== null && typeof value !== 'string') {
+    //         window.location.href = value.link;
+    //     }
+    // }
 
 
-    useEffect(() => {
-        const getDyads = async () => {
-            const response1 = await genericGet('/api/published_works');
-            const response2 = await genericGet('/api/users');
-            const newDyads: dyad[] = [];
-            if (response2) {
-                response2.forEach((user: User) => {
-                    newDyads.push({ isWork: false, name: user.userName, link: `/profile/${user.userName}` });
-                });
-            }
-            if (response1) {
-                response1.forEach((work: Work) => {
-                    newDyads.push({ isWork: true, name: work.title, link: `/story/${work.url}` });
-                });
-            }
+    // useEffect(() => {
+    //     const getDyads = async () => {
+    //         const response1 = await genericGet('/api/published_works');
+    //         const response2 = await genericGet('/api/users');
+    //         const newDyads: dyad[] = [];
+    //         if (response2) {
+    //             response2.forEach((user: User) => {
+    //                 newDyads.push({ isWork: false, name: user.userName, link: `/profile/${user.userName}` });
+    //             });
+    //         }
+    //         if (response1) {
+    //             response1.forEach((work: Work) => {
+    //                 newDyads.push({ isWork: true, name: work.title, link: `/story/${work.url}` });
+    //             });
+    //         }
 
-            setDyads(newDyads);
+    //         setDyads(newDyads);
 
-        }
+    //     }
 
-        getDyads();
+    //     getDyads();
 
-    }, []);
+    // }, []);
 
 
-    const searchIcon: JSX.Element = isSearching ?
-        <Box display="flex">
-            <IconButton action={openSearch} icon={isSearching ? 'icons/search_color.svg'  : '/icons/search.svg'} color={isSearching ? "litegreen" : "green"} />
+    // const searchIcon: JSX.Element = isSearching ?
+    //     <Box display="flex">
+            // <IconButton action={openSearch} icon={isSearching ? 'icons/search_color.svg'  : '/icons/search.svg'} color={isSearching ? "litegreen" : "green"} />
 
-            <Autocomplete autoHighlight sx={{marginLeft:"10px",width:"73%"}} options={dyads} freeSolo={true} onClose={closeSearch} onChange={onChange}
-                getOptionLabel={(option: string | dyad) => {
-                    if (typeof option === 'string') {
-                        return option;
-                    } else {
-                        return option.name;
-                    }
-                }
-                }
-                renderOption={(props, option) => {
-                    return (
-                        <>
-                            <AutocompleteOption {...props}>
-                                <div className="options" style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', gap: '0px'}}>
-                                    <span style={{ color: "#9E9FEB", fontStyle: "Oxanium", fontSize: "18px", padding:'5px 10px 0px'  }}>{option.name}</span>
-                                    <Typography level="body3" sx={{ margin: '1px' }}>
-                                        <span style={{ color: "#9E9FEB", fontStyle: "Oxanium", fontSize: "12px",padding:'5px 10px 0px' }}>{option.isWork ? "Story" : "User"}</span>
-                                    </Typography>
-                                </div>
-                            </AutocompleteOption>
-                        </>
-                    )
-                }}
-            />
-        </Box>
-        :
-        <IconButton action={openSearch} icon='/icons/search.svg' color="green" />;
+    //         <Autocomplete autoHighlight sx={{marginLeft:"10px",width:"73%"}} options={dyads} freeSolo={true} onClose={closeSearch} onChange={onChange}
+    //             getOptionLabel={(option: string | dyad) => {
+    //                 if (typeof option === 'string') {
+    //                     return option;
+    //                 } else {
+    //                     return option.name;
+    //                 }
+    //             }
+    //             }
+    //             renderOption={(props, option) => {
+    //                 return (
+    //                     <>
+    //                         <AutocompleteOption {...props}>
+    //                             <div className="options" style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', gap: '0px'}}>
+    //                                 <span style={{ color: "#9E9FEB", fontStyle: "Oxanium", fontSize: "18px", padding:'5px 10px 0px'  }}>{option.name}</span>
+    //                                 <Typography level="body3" sx={{ margin: '1px' }}>
+    //                                     <span style={{ color: "#9E9FEB", fontStyle: "Oxanium", fontSize: "12px",padding:'5px 10px 0px' }}>{option.isWork ? "Story" : "User"}</span>
+    //                                 </Typography>
+    //                             </div>
+    //                         </AutocompleteOption>
+    //                     </>
+    //                 )
+    //             }}
+    //         />
+    //     </Box>
+    //     :
+    //     <IconButton action={openSearch} icon='/icons/search.svg' color="green" />;
+    <IconButton action={openSearch} icon={isSearching ? 'icons/search_color.svg'  : '/icons/search.svg'} color={isSearching ? "litegreen" : "green"} />
 
 
     return (
         <>
 
             <div className="parent-custom-button">
+
                 <IconButton action={() => {
                     setOpen(!open);
 
@@ -137,15 +145,16 @@ const Hamburger = () => {
                     open={open}
                     aria-labelledby="basic-demo-button"
                     className="basics-demo-button"
-                    style={{ top: "89px", left: "1px", position: "absolute",width: "95.5%"}}
+                    style={{ top: "89px", left: "1px", position: "absolute",width: "95.5%", paddingBottom:'20px'}}
                 >
-                    <MenuItem style={{outline:"none"}}><span className='search-icon'>{searchIcon}</span></MenuItem>
-                    <MenuItem><a href="/home">home</a></MenuItem>
+                    <MenuItem style={{outline:"none"}}><span className='search-icon'><Search/></span></MenuItem>
+                    <MenuItem><a href="/">home</a></MenuItem>
                     <MenuItem><a href="/art">art</a></MenuItem>
                     <MenuItem><a onClick={() => setOpenCollab(true)}>collab</a></MenuItem>
                     <MenuItem><a onClick={createNav} style={{ marginRight: "30px" }}>create</a></MenuItem>
                 </Menu>
             </div>
+            <CollabPopup open={openCollab} close={() => setOpenCollab(false)} />
         </>
     )
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,6 +11,7 @@ import { Work, User} from '../utils/types.ts';
 import { genericGet } from '../utils/api.ts';
 import './Navbar.css';
 import Hamburger from './Hamburger.tsx';
+import Search from './Search.tsx';
 
 
 
@@ -34,9 +35,9 @@ const NavDiv = styled.div`
     max-width: 15vw;
   }
 }
-@media screen and (max-width: 700px) and (min-width: 300px) {
+@media screen and (max-width: 800px) and (min-width: 300px) {
     .nav-items-names a {
-      font-size: 43px;
+      font-size: 30px;
       margin-left: 5px !important;
       margin-right: 5px !important;
       padding-left: 0px !important;;
@@ -48,19 +49,19 @@ const NavDiv = styled.div`
     align-items: center;
 }`;
 
-type dyad = {
-    isWork: boolean;
-    name: string;
-    link: string;
-}
+// type dyad = {
+//     isWork: boolean;
+//     name: string;
+//     link: string;
+// }
 
 function Navbar() {
     const context: object = useContext(UserContext);
     const [open, setOpen] = useState<boolean>(false);
     const [openCreator, setOpenCreator] = useState<boolean>(false);
     const [openCollab, setOpenCollab] = useState<boolean>(false);
-    const [isSearching , setIsSearching] = useState<boolean>(false);
-    const [dyads, setDyads] = useState<dyad[]>([]);
+    // const [isSearching , setIsSearching] = useState<boolean>(false);
+    // const [dyads, setDyads] = useState<dyad[]>([]);
     const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
     const blockAccess = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         e.preventDefault();
@@ -89,71 +90,71 @@ function Navbar() {
         setOpenCreator(false);
     }
 
-    const openSearch = async () => {
-        setIsSearching(true);
-    } 
+    
+    //     setIsSearching(true);
+    // } 
 
-    const closeSearch = async () => {
-        setIsSearching(false);
-    }
+    // const closeSearch = async () => {
+    //     setIsSearching(false);
+    // }
 
-    const onChange = async (e: React.SyntheticEvent<Element, Event>, value: string | dyad | null) => {
-        console.log(value);
-        if (value !== null && typeof value !== 'string') {
-            window.location.href = value.link;
-        }
-    }
+    // const onChange = async (e: React.SyntheticEvent<Element, Event>, value: string | dyad | null) => {
+    //     console.log(value);
+    //     if (value !== null && typeof value !== 'string') {
+    //         window.location.href = value.link;
+    //     }
+    // }
 
 
-    useEffect(() => {
-        const getDyads = async () => {
-            const response1 = await genericGet('/api/published_works');
-            const response2 = await genericGet('/api/users');
-            const newDyads: dyad[] = [];
-            if (response2){
-                response2.forEach((user: User) => {
-                    newDyads.push({isWork: false, name: user.userName, link: `/profile/${user.userName}`});
-                });
-            }
-            if (response1){ 
-                response1.forEach((work: Work) => {
-                    newDyads.push({isWork: true, name: work.title, link: `/story/${work.url}`});
-                });
-            }
+    // useEffect(() => {
+    //     const getDyads = async () => {
+    //         const response1 = await genericGet('/api/published_works');
+    //         const response2 = await genericGet('/api/users');
+    //         const newDyads: dyad[] = [];
+    //         if (response2){
+    //             response2.forEach((user: User) => {
+    //                 newDyads.push({isWork: false, name: user.userName, link: `/profile/${user.userName}`});
+    //             });
+    //         }
+    //         if (response1){ 
+    //             response1.forEach((work: Work) => {
+    //                 newDyads.push({isWork: true, name: work.title, link: `/story/${work.url}`});
+    //             });
+    //         }
             
-            setDyads(newDyads);
+    //         setDyads(newDyads);
 
-        }
+    //     }
 
-        getDyads();
+    //     getDyads();
         
-    }, []);
+    // }, []);
 
-    const searchIcon: JSX.Element = isSearching ?
-        <Autocomplete autoHighlight sx= {{width: "190px", marginRight:'20px'}} options = {dyads} freeSolo={true} onClose={closeSearch} onChange={onChange} 
-        getOptionLabel={(option: string | dyad) => {
-            if (typeof option === 'string') {
-                return option;
-                } else {
-                    return option.name;
-                }
-            }
-        }
-        renderOption={(props, option) => {
-            return (
-                <AutocompleteOption {...props}>
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'left', gap: '0px'}}>
-                    {option.name}
-                    <Typography level="body3" sx={{margin: '1px'}}>
-                    {option.isWork ? "Story" : "User"}
-                    </Typography>
-                </div>
-                </AutocompleteOption>
-            )
-        }}
-        />
-        :
-        <IconButton action={openSearch} icon='/icons/search.svg' color="green" />;
+    // const searchIcon: JSX.Element = isSearching ?
+    //     <Autocomplete autoHighlight sx= {{width: "225px"}} options = {dyads} freeSolo={true} onClose={closeSearch} onChange={onChange} 
+    //     getOptionLabel={(option: string | dyad) => {
+    //         if (typeof option === 'string') {
+    //             return option;
+    //             } else {
+    //                 return option.name;
+    //             }
+    //         }
+    //     }
+    //     renderOption={(props, option) => {
+    //         return (
+    //             <AutocompleteOption {...props}>
+    //             <div style={{display: 'flex', flexDirection: 'column', alignItems: 'left', gap: '0px'}}>
+    //                 {option.name}
+    //                 <Typography level="body3" sx={{margin: '1px'}}>
+    //                 {option.isWork ? "Story" : "User"}
+    //                 </Typography>
+    //             </div>
+    //             </AutocompleteOption>
+    //         )
+    //     }}
+    //     />
+    //     :
+    //     <IconButton action={openSearch} icon='/icons/search.svg' color="green" />;
 
     return (
         <div style={{marginBottom: '25px'}}>
@@ -173,7 +174,7 @@ function Navbar() {
                         <a onClick={createNav} style={{marginRight:"30px"}}>create</a>
                     </Box>
                     <Box className='icon-search nav-items-names'>
-                    <a className="searchh">{searchIcon}</a>
+                    <a className="search"><Search/></a>
                         <a style={{paddingLeft:"10px",paddingTop: "10px"}}>
                             <ClickProfile
                                 isLoggedIn={true}
@@ -203,7 +204,7 @@ function Navbar() {
                         <a onClick={blockAccess} style={{marginRight:"30px"}}>create</a>
                         </Box>
                         <Box className='icon-search nav-items-names'>
-                        <a className="searchh">{searchIcon}</a>
+                        <a className="search"><Search/></a>
                         <a style={{padding:"0px 10px"}}>
                             < ClickProfile
                                 isLoggedIn={false}
