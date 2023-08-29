8538ccd5cc053a0cdbf6f810af3d5c4b72faf70f
BCassady <bryant.cassady@gmail.com>
2023-08-28 23:08:55 -0500

    Switch minting to using S3

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@ export const PROFILE_IMGS_BUCKET: String = 'twine-'+env+'-profile-imgs';
 export const CHAPTER_IMGS_BUCKET: String = 'twine-'+env+'-chapter-imgs';
 export const STORY_IMGS_BUCKET: String = 'twine-'+env+'-story-imgs';
 export const FEEDBACK_IMGS_BUCKET: String = 'twine-'+env+'-feedback-imgs';
+export const MINT_IMGS_BUCKET: String = 'twine-mint-imgs';
