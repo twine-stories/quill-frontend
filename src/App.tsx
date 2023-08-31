0f8582c26245570eeb6db3b94e066bd49d102908
Rithik Jain <rithikj7200@gmail.com>
2023-08-30 21:33:53 -0500

    Create new beta wall (#106)
    * Create new beta wall
    
    * Prettified Code!
    
    * Fix small ui issue
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: Rithik <Jain>
    Co-authored-by: rithikjain1480 <rithikjain1480@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -73,9 +73,6 @@ export const peraWallet = new PeraWalletConnect({
     chainId: chainId,
 })
 
-// probably move to secrets manager but this doesn't really need to be that secure
-const accessCode: string = 'twinebeta!!'
-
 function App() {
     const [user, setUser] = useState<User>()
     const [address, setAddress] = useState<string>()
@@ -83,7 +80,7 @@ function App() {
     const [getUserToggle, setGetUserToggle] = useState<boolean>(false)
     const [initUserLoad, setInitUserLoad] = useState<boolean>(false)
     const [connType, setConnType] = useState<ConnectType>(ConnectType.PERA)
-    const [beta, setBeta] = useState<boolean>(true)
+    const [needPass, setNeedPass] = useState<boolean>(false)
     const [usePera, setUsePera] = useState<boolean>(false)
     const [useMyAlgo, setUseMyAlgo] = useState<boolean>(false)
 
@@ -92,7 +89,7 @@ function App() {
             peraWallet.disconnect()
         }
         deleteCookie('session')
-        setUser(null)
+        setUser(undefined)
     }
 
     const setUserCookie = (walletAddress: string, cookie: string) => {
@@ -122,7 +119,7 @@ function App() {
         return false
     }
 
-    const connectToMyAlgo = async (): Promise<void> => {
+    const connectToMyAlgo = async (checkForPass: boolean): Promise<void> => {
         try {
             setConnType(ConnectType.MY_ALGO)
             const accounts = await reach.getDefaultAccount()
@@ -131,7 +128,12 @@ function App() {
                 ConnectType.MY_ALGO
             )
             if (shouldContinue) {
-                onComplete(accounts['networkAccount']['addr'])
+                const account = accounts['networkAccount']['addr']
+                if (checkForPass) {
+                    checkForTwinePass(account)
+                } else {
+                    onComplete(account)
+                }
             } else {
                 setUsePera(true)
             }
@@ -140,7 +142,7 @@ function App() {
         }
     }
 
-    const connectToPera = async (): Promise<void> => {
+    const connectToPera = async (checkForPass: boolean): Promise<void> => {
         try {
             setConnType(ConnectType.PERA)
             const newAccounts = await peraWallet.connect()
@@ -150,7 +152,12 @@ function App() {
                 ConnectType.PERA
             )
             if (shouldContinue) {
-                onComplete(newAccounts[0])
+                const account = newAccounts[0]
+                if (checkForPass) {
+                    checkForTwinePass(account, true)
+                } else {
+                    onComplete(account)
+                }
             } else {
                 peraWallet.disconnect()
                 setUseMyAlgo(true)
@@ -158,6 +165,30 @@ function App() {
         } catch (err) {
             console.error(err)
         }
+    }
+
+    const checkForTwinePass = (addr: string, isPera?: boolean): void => {
+        let assetId: string
+        if (env === 'prod') {
+            // assetId = "1180056515"
+            assetId = '1'
+        } else {
+            assetId = '275215927'
+        }
+
+        genericGet('/api/algo/has-asset/' + addr + '/' + assetId).then(
+            (response: boolean) => {
+                console.log(response)
+                if (response) {
+                    onComplete(addr)
+                } else {
+                    if (isPera) {
+                        peraWallet.disconnect()
+                    }
+                    setNeedPass(true)
+                }
+            }
+        )
     }
 
     const getAndSetUser = (addr: string): void => {
@@ -173,7 +204,7 @@ function App() {
         const cookie = uuidv4()
         const newUser: User = {
             walletAddress: walletAddress,
-            email: null,
+            email: undefined,
             firstName: firstName,
             lastName: lastName,
             profileImg: 'default.jpeg',
@@ -197,20 +228,6 @@ function App() {
         setAddress('')
     }
 
-    const enterBeta = (code: string) => {
-        if (code === accessCode) {
-            setCookie('beta_session', 'active')
-            setBeta(false)
-        }
-    }
-
-    useEffect(() => {
-        const cookie = getCookie('beta_session')
-        if (cookie === 'active') {
-            setBeta(false)
-        }
-    }, [])
-
     useEffect(() => {
         const cookie = getCookie('session')
         if (cookie === '') {
@@ -229,7 +246,9 @@ function App() {
         }
         genericGet('/api/user/cookie/' + cookie).then(
             (response: User | null) => {
-                setUser(response)
+                if (response) {
+                    setUser(response)
+                }
             }
         )
     }, [getUserToggle])
@@ -298,7 +317,6 @@ function App() {
                         closeLogin: cancelLogin,
                         addUser: addUser,
                         updateUser: setUser,
-                        enterBeta: enterBeta,
                     }}
                 >
                     <FirstLogin />
@@ -316,7 +334,14 @@ function App() {
                         }}
                         message="Your account is associated with MyAlgo Wallet. Please log in with MyAlgo Wallet instead."
                     />
-                    {beta ? (
+                    <ErrorPopup
+                        isOpen={needPass}
+                        onClose={() => {
+                            setNeedPass(false)
+                        }}
+                        message="You need to have a Twine Pass in your wallet to access the site!"
+                    />
+                    {!user ? (
                         <Routes>
                             <Route path="/*" element={<Beta />}></Route>
                         </Routes>
--- src/components/ClickProfile.tsx
+++ src/components/ClickProfile.tsx
@@ -1,28 +1,31 @@
 import './ClickProfile.css'
-import {
-    Menu,
-    MenuItem,
-    Modal,
-    Sheet,
-    Grid,
-    ModalClose,
-    Typography,
-    Box,
-} from '@mui/joy'
+import { Menu, MenuItem, Modal, Sheet, Grid, Typography, Box } from '@mui/joy'
 import React, { useContext, useState } from 'react'
 import { User } from '../types'
 import { UserContext } from '../App.tsx'
 import { useNavigate } from 'react-router-dom'
 import { String } from 'aws-sdk/clients/cloudhsm'
-import IconButton from './IconButton.tsx'
 import { PROFILE_IMGS_BUCKET } from '../config.ts'
 
-export default function ClickProfile({
-    isLoggedIn,
-    logOutFunc,
-    connectAlgoFunc,
-    connectPeraFunc,
-}) {
+interface ClickProfileProps {
+    isLoggedIn: boolean
+    logOutFunc: () => void
+    connectAlgoFunc: (checkForPass: boolean) => Promise<void>
+    connectPeraFunc: (checkForPass: boolean) => Promise<void>
+    checkForPass: boolean
+    displayText?: string
+}
+
+export default function ClickProfile(props: ClickProfileProps) {
+    const {
+        isLoggedIn,
+        logOutFunc,
+        connectAlgoFunc,
+        connectPeraFunc,
+        checkForPass,
+        displayText,
+    } = props
+
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
     const open = Boolean(anchorEl)
     const context: object = useContext(UserContext)
@@ -42,9 +45,9 @@ export default function ClickProfile({
             logOutFunc()
             navigate('/')
         } else if (button == 'algo') {
-            connectAlgoFunc()
+            connectAlgoFunc(checkForPass)
         } else if (button == 'pera') {
-            connectPeraFunc()
+            connectPeraFunc(checkForPass)
         }
     }
 
@@ -90,10 +93,16 @@ export default function ClickProfile({
                     </Menu>
                 </div>
             ) : (
-                <div className="navBar-login">
-                    <a id="navbarLogin" onClick={handleClick}>
-                        login
-                    </a>
+                <div className="navbar-login">
+                    <Typography
+                        level="h2"
+                        color={checkForPass ? 'purple' : 'green'}
+                        id="navbar-login"
+                    >
+                        <a onClick={handleClick}>
+                            {displayText ? displayText : 'login'}
+                        </a>
+                    </Typography>
                     <Modal open={open} onClose={() => handleClose('')}>
                         <Sheet
                             variant="outlined"
