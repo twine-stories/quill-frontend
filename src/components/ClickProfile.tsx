7db08719b5f88bbf364830c836895cca8652b977
rithikjain1480 <rithikjain1480@users.noreply.github.com>
2023-08-31 02:04:51 +0000

    Prettified Code!

--- src/components/ClickProfile.tsx
+++ src/components/ClickProfile.tsx
@@ -1,13 +1,5 @@
 import './ClickProfile.css'
-import {
-    Menu,
-    MenuItem,
-    Modal,
-    Sheet,
-    Grid,
-    Typography,
-    Box,
-} from '@mui/joy'
+import { Menu, MenuItem, Modal, Sheet, Grid, Typography, Box } from '@mui/joy'
 import React, { useContext, useState } from 'react'
 import { User } from '../types'
 import { UserContext } from '../App.tsx'
@@ -25,7 +17,14 @@ interface ClickProfileProps {
 }
 
 export default function ClickProfile(props: ClickProfileProps) {
-    const { isLoggedIn, logOutFunc, connectAlgoFunc, connectPeraFunc, checkForPass, displayText } = props
+    const {
+        isLoggedIn,
+        logOutFunc,
+        connectAlgoFunc,
+        connectPeraFunc,
+        checkForPass,
+        displayText,
+    } = props
 
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
     const open = Boolean(anchorEl)
@@ -95,9 +94,9 @@ export default function ClickProfile(props: ClickProfileProps) {
                 </div>
             ) : (
                 <div className="navbar-login">
-                    <Typography level='h2' color='purple' id="navbar-login">
+                    <Typography level="h2" color="purple" id="navbar-login">
                         <a onClick={handleClick}>
-                            {displayText ? displayText : "login"}
+                            {displayText ? displayText : 'login'}
                         </a>
                     </Typography>
                     <Modal open={open} onClose={() => handleClose('')}>
